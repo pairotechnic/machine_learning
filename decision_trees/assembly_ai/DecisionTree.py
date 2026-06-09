f96f243e835878abf84eeda70b10ea459d0ec773
Rohit Pai <rohitkalsankpai@gmail.com>
2026-06-09 14:29:03 +0530

    Decision Tree experiment

---
+++ decision_trees/assembly_ai/DecisionTree.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_features = n_features
+        self.root = None
+
+    def fit(self, X, y):
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
+        self.root = self._grow_tree(X, y)
+
+    def predict():
+        pass
