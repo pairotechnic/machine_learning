3648a560d00b1eca951b3361c15625c19b2c5cff
Rohit Pai <rohitkalsankpai@gmail.com>
2026-08-03 11:35:30 +0530

    Cost function representation, when w is the only variable its 2d parabloa, and when w and b are both variable its 3d soup bowl surface plot

---
+++ machine_learning_specialization/supervised/regression/cost_function.py
@@ -0,0 +1,57 @@
+import numpy as np
+# matplotlib.use("TkAgg") # or "QtAgg"
+import matplotlib
+import matplotlib.pyplot as plt
+print(f"matplotlib backend : {matplotlib.get_backend()}")
+
+# works only in Jupyter Notebook environments
+# %matplotlib widget 
+plt.style.use(".\\machine_learning_specialization\\supervised\\regression\\deeplearning.mplstyle")
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+
+x_train = np.array([1.0, 2.0]) # unit of size is 1000 sq.ft.
+y_train = np.array([300.0, 500.0]) # unit of price is 1000 dollars
+
+def compute_cost(x, y, w, b):
+    """
+    Computes the cost function for linear regression.
+
+    Args:
+        x (ndarray (m, )): Input values, m examples
+        y (ndarray (m, )): Target values, m examples
+        w, b (scalar): model parameters
+
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0]
+
+    cost_sum = 0
+    for i in range(m):
+        f_wb = w*x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        cost_sum += cost
+
+    total_cost = (1/(2*m)) * cost_sum
+
+    return total_cost
+
+# Comes from a local import in the course's Cost Function Jupyter Notebook 
+# b is fixed, only w variable, creates 2d cost function
+plt_intuition(x_train,y_train)
+
+# Larger data set that doesn't exactly fit a line
+x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
+y_train = np.array([250, 300, 480, 430, 630, 730,])
+
+# w and b both variables
+# creates 3d cost function
+# approximately  𝑤=209 and  𝑏=2.4 provide lowest cost
+plt.close('all')
+fig, ax, dyn_items = plt_stationary(x_train, y_train)
+updater = plt_update_onclick(fig, ax, x_train, y_train, dyn_items)
+
+# Another 3d surface plot (Convex cost durface)
+soup_bowl()
+
