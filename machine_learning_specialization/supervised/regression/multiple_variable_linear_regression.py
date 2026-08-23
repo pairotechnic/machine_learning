55f82b8bbb7434b9aac73787dfb635c9caeb81b4
Rohit Pai <rohitkalsankpai@gmail.com>
2026-08-23 11:07:43 +0530

    Fixed imports, and refactored into functions

--- machine_learning_specialization/supervised/regression/multiple_variable_linear_regression.py
+++ machine_learning_specialization/supervised/regression/multiple_variable_linear_regression.py
@@ -1,33 +1,25 @@
+# Standard Library Imports
 import copy, math
+
+# Third-party Library Imports
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('./deeplearning.mplstyle')
+
+# Local Application Imports
+plt.style.use(".\\machine_learning_specialization\\supervised\\regression\\utils\\deeplearning.mplstyle")
 np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
 
-X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
-y_train = np.array([460, 232, 178])
-
-# data is stored in numpy array/matrix
-print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
-print(X_train)
-print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
-print(y_train)
-
-b_init = 785.1811367994083
-w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
-print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
-
-def predict_single_loop(x, w, b): 
+def predict_with_for_loop(x, w, b): 
     """
     single predict using linear regression
     
     Args:
-      x (ndarray): Shape (n,) example with multiple features
-      w (ndarray): Shape (n,) model parameters    
-      b (scalar):  model parameter     
-      
+    x (ndarray): Shape (n,) example with multiple features
+    w (ndarray): Shape (n,) model parameters    
+    b (scalar):  model parameter     
+    
     Returns:
-      p (scalar):  prediction
+    p (scalar):  prediction
     """
     n = x.shape[0]
     p = 0
@@ -37,47 +29,31 @@ def predict_single_loop(x, w, b):
     p = p + b                
     return p
 
-# get a row from our training data
-x_vec = X_train[0,:]
-print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
-
-# make a prediction
-f_wb = predict_single_loop(x_vec, w_init, b_init)
-print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
-
-def predict(x, w, b): 
+def predict_with_dot_product(x, w, b): 
     """
     single predict using linear regression
     Args:
-      x (ndarray): Shape (n,) example with multiple features
-      w (ndarray): Shape (n,) model parameters   
-      b (scalar):             model parameter 
-      
+    x (ndarray): Shape (n,) example with multiple features
+    w (ndarray): Shape (n,) model parameters   
+    b (scalar):             model parameter 
+    
     Returns:
-      p (scalar):  prediction
+    p (scalar):  prediction
     """
     p = np.dot(x, w) + b     
     return p    
-
-# get a row from our training data
-x_vec = X_train[0,:]
-print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
-
-# make a prediction
-f_wb = predict(x_vec,w_init, b_init)
-print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
 
 def compute_cost(X, y, w, b): 
     """
     compute cost
     Args:
-      X (ndarray (m,n)): Data, m examples with n features
-      y (ndarray (m,)) : target values
-      w (ndarray (n,)) : model parameters  
-      b (scalar)       : model parameter
-      
+    X (ndarray (m,n)): Data, m examples with n features
+    y (ndarray (m,)) : target values
+    w (ndarray (n,)) : model parameters  
+    b (scalar)       : model parameter
+    
     Returns:
-      cost (scalar): cost
+    cost (scalar): cost
     """
     m = X.shape[0]
     cost = 0.0
@@ -87,22 +63,18 @@ def compute_cost(X, y, w, b):
     cost = cost / (2 * m)                      #scalar    
     return cost
 
-# Compute and display cost using our pre-chosen optimal parameters. 
-cost = compute_cost(X_train, y_train, w_init, b_init)
-print(f'Cost at optimal w : {cost}')
-
 def compute_gradient(X, y, w, b): 
     """
     Computes the gradient for linear regression 
     Args:
-      X (ndarray (m,n)): Data, m examples with n features
-      y (ndarray (m,)) : target values
-      w (ndarray (n,)) : model parameters  
-      b (scalar)       : model parameter
-      
+    X (ndarray (m,n)): Data, m examples with n features
+    y (ndarray (m,)) : target values
+    w (ndarray (n,)) : model parameters  
+    b (scalar)       : model parameter
+    
     Returns:
-      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
-      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
+    dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+    dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
     """
     m,n = X.shape           #(number of examples, number of features)
     dj_dw = np.zeros((n,))
@@ -118,30 +90,25 @@ def compute_gradient(X, y, w, b):
         
     return dj_db, dj_dw
 
-#Compute and display gradient 
-tmp_dj_db, tmp_dj_dw = compute_gradient(X_train, y_train, w_init, b_init)
-print(f'dj_db at initial w,b: {tmp_dj_db}')
-print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
-
 def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
     """
     Performs batch gradient descent to learn w and b. Updates w and b by taking 
     num_iters gradient steps with learning rate alpha
     
     Args:
-      X (ndarray (m,n))   : Data, m examples with n features
-      y (ndarray (m,))    : target values
-      w_in (ndarray (n,)) : initial model parameters  
-      b_in (scalar)       : initial model parameter
-      cost_function       : function to compute cost
-      gradient_function   : function to compute the gradient
-      alpha (float)       : Learning rate
-      num_iters (int)     : number of iterations to run gradient descent
-      
+    X (ndarray (m,n))   : Data, m examples with n features
+    y (ndarray (m,))    : target values
+    w_in (ndarray (n,)) : initial model parameters  
+    b_in (scalar)       : initial model parameter
+    cost_function       : function to compute cost
+    gradient_function   : function to compute the gradient
+    alpha (float)       : Learning rate
+    num_iters (int)     : number of iterations to run gradient descent
+    
     Returns:
-      w (ndarray (n,)) : Updated values of parameters 
-      b (scalar)       : Updated value of parameter 
-      """
+    w (ndarray (n,)) : Updated values of parameters 
+    b (scalar)       : Updated value of parameter 
+    """
     
     # An array to store cost J and w's at each iteration primarily for graphing later
     J_history = []
@@ -156,7 +123,7 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, 
         # Update Parameters using w, b, alpha and gradient
         w = w - alpha * dj_dw               ##None
         b = b - alpha * dj_db               ##None
-      
+    
         # Save cost J at each iteration
         if i<100000:      # prevent resource exhaustion 
             J_history.append( cost_function(X, y, w, b))
@@ -167,26 +134,79 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, 
         
     return w, b, J_history #return final w,b and J history for graphing
 
-# initialize parameters
-initial_w = np.zeros_like(w_init)
-initial_b = 0.
-# some gradient descent settings
-iterations = 1000
-alpha = 5.0e-7
-# run gradient descent 
-w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
-                                                    compute_cost, compute_gradient, 
-                                                    alpha, iterations)
-print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
-m,_ = X_train.shape
-for i in range(m):
-    print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
 
-# plot cost versus iteration  
-fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
-ax1.plot(J_hist)
-ax2.plot(100 + np.arange(len(J_hist[100:])), J_hist[100:])
-ax1.set_title("Cost vs. iteration");  ax2.set_title("Cost vs. iteration (tail)")
-ax1.set_ylabel('Cost')             ;  ax2.set_ylabel('Cost') 
-ax1.set_xlabel('iteration step')   ;  ax2.set_xlabel('iteration step') 
-plt.show()
+def main():
+    X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+    y_train = np.array([460, 232, 178])
+
+    # data is stored in numpy array/matrix
+    print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+    print(X_train)
+    print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+    print(y_train)
+
+    b_init = 785.1811367994083
+    w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+    print(f"w_init shape: {w_init.shape}, w_init type: {type(w_init)}")
+    print(f"b_init type: {type(b_init)}")
+
+    # get a row from our training data
+    x_vec = X_train[0,:]
+    print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+    # make a prediction
+    f_wb = predict_with_for_loop(x_vec, w_init, b_init)
+    print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+    # get a row from our training data
+    x_vec = X_train[0,:]
+    print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+    # make a prediction
+    f_wb = predict_with_dot_product(x_vec,w_init, b_init)
+    print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+    # Compute and display cost using our pre-chosen optimal parameters. 
+    cost = compute_cost(X_train, y_train, w_init, b_init)
+    print(f'Cost at optimal w : {cost}')
+
+    #Compute and display gradient 
+    tmp_dj_db, tmp_dj_dw = compute_gradient(X_train, y_train, w_init, b_init)
+    print(f'dj_db at initial w,b: {tmp_dj_db}')
+    print(f'dj_dw at initial w,b: {tmp_dj_dw}')
+
+    # initialize parameters
+    initial_w = np.zeros_like(w_init)
+    initial_b = 0.
+
+    # some gradient descent settings
+    iterations = 1000
+    alpha = 5.0e-7
+
+    # run gradient descent 
+    w_final, b_final, J_hist = gradient_descent(
+        X_train, 
+        y_train, 
+        initial_w, 
+        initial_b, 
+        compute_cost, 
+        compute_gradient, 
+        alpha, 
+        iterations
+    )
+    print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+    m,_ = X_train.shape
+    for i in range(m):
+        print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
+
+    # plot cost versus iteration  
+    fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
+    ax1.plot(J_hist)
+    ax2.plot(100 + np.arange(len(J_hist[100:])), J_hist[100:])
+    ax1.set_title("Cost vs. iteration");  ax2.set_title("Cost vs. iteration (tail)")
+    ax1.set_ylabel('Cost')             ;  ax2.set_ylabel('Cost') 
+    ax1.set_xlabel('iteration step')   ;  ax2.set_xlabel('iteration step') 
+    plt.show()
+
+if __name__ == "__main__":
+    main()
