66afef53138a3a56a28d13fd96bbdb15ad2a3866
Rohit Pai <rohitkalsankpai@gmail.com>
2026-09-17 18:41:50 +0530

    1. Added scikit-learn to requirements 2. Organized and fixed imports 3. Moved code into main function

--- machine_learning_specialization/supervised/regression/scikit_learn_linear_regression.py
+++ machine_learning_specialization/supervised/regression/scikit_learn_linear_regression.py
@@ -1,45 +1,56 @@
-import numpy as np
+# Standard Library Imports
+
+# Third-Party Library Imports
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
-from lab_utils_multi import  load_house_data
-from lab_utils_common import dlc
+
+# Local Application Imports
+from machine_learning_specialization.supervised.regression.utils.lab_utils_common import dlc
+from machine_learning_specialization.supervised.regression.utils.lab_utils_multi import  load_house_data
+
 np.set_printoptions(precision=2)
 plt.style.use('./deeplearning.mplstyle')
 
-X_train, y_train = load_house_data()
-X_features = ['size(sqft)','bedrooms','floors','age']
 
-scaler = StandardScaler()
-X_norm = scaler.fit_transform(X_train)
-print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_train,axis=0)}")   
-print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
+def main():
+    X_train, y_train = load_house_data()
+    X_features = ['size(sqft)','bedrooms','floors','age']
 
-sgdr = SGDRegressor(max_iter=1000)
-sgdr.fit(X_norm, y_train)
-print(sgdr)
-print(f"number of iterations completed: {sgdr.n_iter_}, number of weight updates: {sgdr.t_}")
+    scaler = StandardScaler()
+    X_norm = scaler.fit_transform(X_train)
+    print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_train,axis=0)}")   
+    print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
 
-b_norm = sgdr.intercept_
-w_norm = sgdr.coef_
-print(f"model parameters:                   w: {w_norm}, b:{b_norm}")
-print( "model parameters from previous lab: w: [110.56 -21.27 -32.71 -37.97], b: 363.16")
+    sgdr = SGDRegressor(max_iter=1000)
+    sgdr.fit(X_norm, y_train)
+    print(sgdr)
+    print(f"number of iterations completed: {sgdr.n_iter_}, number of weight updates: {sgdr.t_}")
 
-# make a prediction using sgdr.predict()
-y_pred_sgd = sgdr.predict(X_norm)
-# make a prediction using w,b. 
-y_pred = np.dot(X_norm, w_norm) + b_norm  
-print(f"prediction using np.dot() and sgdr.predict match: {(y_pred == y_pred_sgd).all()}")
+    b_norm = sgdr.intercept_
+    w_norm = sgdr.coef_
+    print(f"model parameters:                   w: {w_norm}, b:{b_norm}")
+    print( "model parameters from previous lab: w: [110.56 -21.27 -32.71 -37.97], b: 363.16")
 
-print(f"Prediction on training set:\n{y_pred[:4]}" )
-print(f"Target values \n{y_train[:4]}")
+    # make a prediction using sgdr.predict()
+    y_pred_sgd = sgdr.predict(X_norm)
+    # make a prediction using w,b. 
+    y_pred = np.dot(X_norm, w_norm) + b_norm  
+    print(f"prediction using np.dot() and sgdr.predict match: {(y_pred == y_pred_sgd).all()}")
 
-# plot predictions and targets vs original features    
-fig,ax=plt.subplots(1,4,figsize=(12,3),sharey=True)
-for i in range(len(ax)):
-    ax[i].scatter(X_train[:,i],y_train, label = 'target')
-    ax[i].set_xlabel(X_features[i])
-    ax[i].scatter(X_train[:,i],y_pred,color=dlc["dlorange"], label = 'predict')
-ax[0].set_ylabel("Price"); ax[0].legend();
-fig.suptitle("target versus prediction using z-score normalized model")
-plt.show()
+    print(f"Prediction on training set:\n{y_pred[:4]}" )
+    print(f"Target values \n{y_train[:4]}")
+
+    # plot predictions and targets vs original features    
+    fig,ax=plt.subplots(1,4,figsize=(12,3),sharey=True)
+    for i in range(len(ax)):
+        ax[i].scatter(X_train[:,i],y_train, label = 'target')
+        ax[i].set_xlabel(X_features[i])
+        ax[i].scatter(X_train[:,i],y_pred,color=dlc["dlorange"], label = 'predict')
+    ax[0].set_ylabel("Price"); ax[0].legend();
+    fig.suptitle("target versus prediction using z-score normalized model")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
